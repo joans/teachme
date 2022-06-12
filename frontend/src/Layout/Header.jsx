55c0d9ca7e3b22583b237369e3364fc8b5040726
Jonas <code@ununun.de>
2022-06-12 11:42:29 +0200

    Add Icons, begin of responsive layout

--- frontend/src/Layout/Header.jsx
+++ frontend/src/Layout/Header.jsx
@@ -2,33 +2,57 @@ import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Button from "../UI/Button";
 import classes from "./Header.module.css";
+import { TiPlus, TiTags } from "react-icons/ti";
+import { CgLogIn } from "react-icons/cg"
 
 const Header = () => {
-    const navigate = useNavigate();
-
+  const navigate = useNavigate();
 
   return (
     <>
       <header className={`${classes.header} color-main`}>
-          <div className={classes["left-side"]}>
-            <h1><Link to="/">Skill Bill</Link></h1>
-          </div>
-          <div className={classes["right-side"]}>
-            <ul>
-              <li>
-                <Link to="/create_offer">Create Offer</Link>
-              </li>
-              <li>
-                <Link to="/categories">Categories</Link>
-              </li>
-              <li>
-                <Button onClick={()=> {navigate("/login")}} to="/login" variant="contained" className="color-two">Log In</Button>
-              </li>
-              <li>
-                <Button onClick={()=> {navigate("/signup")}} className="color-three">Sign Up</Button>
-              </li>
-            </ul>
-          </div>
+        <div className={classes["left-side"]}>
+          <h1>
+            <Link to="/">Skill Bill</Link>
+          </h1>
+        </div>
+        <div className={classes["right-side"]}>
+          <ul>
+            <li>
+              <Link to="/create_offer">
+                <TiPlus className={classes.icon} />
+                Create Offer
+              </Link>
+            </li>
+            <li>
+                <TiTags className={classes.icon} />
+              <Link to="/categories">Categories</Link>
+            </li>
+            <li>
+              <Button
+                onClick={() => {
+                  navigate("/login");
+                }}
+                to="/login"
+                variant="contained"
+                className="color-two"
+              >
+                <CgLogIn className={classes.icon} />
+                Log In
+              </Button>
+            </li>
+            <li>
+              <Button
+                onClick={() => {
+                  navigate("/signup");
+                }}
+                className="color-three"
+              >
+                Sign Up
+              </Button>
+            </li>
+          </ul>
+        </div>
       </header>
     </>
   );
