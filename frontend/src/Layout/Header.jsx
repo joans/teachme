29f5f8de38b0a095c60ed7157f3f9da690a0bf69
Jonas <code@ununun.de>
2022-06-12 17:28:08 +0200

    Implement basic responsive, mobile-first design
    - implement useage of vars where possible

--- frontend/src/Layout/Header.jsx
+++ frontend/src/Layout/Header.jsx
@@ -3,7 +3,8 @@ import { Link, useNavigate } from "react-router-dom";
 import Button from "../UI/Button";
 import classes from "./Header.module.css";
 import { TiPlus, TiTags } from "react-icons/ti";
-import { CgLogIn } from "react-icons/cg"
+import { CgLogIn } from "react-icons/cg";
+import { GiSkills } from "react-icons/gi";
 
 const Header = () => {
   const navigate = useNavigate();
@@ -13,7 +14,10 @@ const Header = () => {
       <header className={`${classes.header} color-main`}>
         <div className={classes["left-side"]}>
           <h1>
-            <Link to="/">Skill Bill</Link>
+            <Link to="/">
+              <GiSkills className={classes.icon} />
+              <span className={classes.text}> Skill Bill</span>
+            </Link>
           </h1>
         </div>
         <div className={classes["right-side"]}>
@@ -21,12 +25,14 @@ const Header = () => {
             <li>
               <Link to="/create_offer">
                 <TiPlus className={classes.icon} />
-                Create Offer
+                <span className={classes.text}>Create Offer</span>
               </Link>
             </li>
             <li>
+              <Link to="/categories">
                 <TiTags className={classes.icon} />
-              <Link to="/categories">Categories</Link>
+                <span className={classes.text}>Categories</span>
+              </Link>
             </li>
             <li>
               <Button
@@ -37,7 +43,7 @@ const Header = () => {
                 variant="contained"
                 className="color-two"
               >
-                <CgLogIn className={classes.icon} />
+                {/* <CgLogIn className={classes.icon} /> */}
                 Log In
               </Button>
             </li>
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,13 +1,67 @@
 import React from "react";
 
 const Home = () => {
-  return<>
-  <div>
-    <h1>Home</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum ratione molestiae blanditiis iure obcaecati delectus. Non, minus dolorum. Sunt necessitatibus repellendus vitae quos ipsum earum voluptates eos molestiae temporibus deserunt?</p>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere magni temporibus fugiat, consequatur beatae ullam ut, error amet aperiam debitis quidem necessitatibus, doloremque voluptate blanditiis veniam omnis maiores esse saepe.</p>
-  </div>
-  </>;
+  return (
+    <>
+      <div>
+        <h1>Home</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
+          ratione molestiae blanditiis iure obcaecati delectus. Non, minus
+          dolorum. Sunt necessitatibus repellendus vitae quos ipsum earum
+          voluptates eos molestiae temporibus deserunt?
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere magni
+          temporibus fugiat, consequatur beatae ullam ut, error amet aperiam
+          debitis quidem necessitatibus, doloremque voluptate blanditiis veniam
+          omnis maiores esse saepe.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
+          ipsum dolor incidunt eum quasi totam, optio voluptas autem temporibus
+          ducimus ipsa rem ullam nostrum praesentium quas, animi voluptate quos
+          vel.
+        </p>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum,
+          voluptas! Fuga, distinctio, nostrum excepturi consequatur minus
+          dolores impedit architecto et eum reprehenderit, exercitationem
+          recusandae? Similique ut aspernatur tempora sunt commodi!
+        </p>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat animi
+          at, pariatur delectus enim veritatis! Doloremque labore quisquam quis
+          corporis harum perferendis, eligendi et! Repudiandae alias voluptate
+          numquam? Explicabo, maxime.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+          exercitationem saepe nemo! Maiores perspiciatis delectus animi
+          repellat odit. Aliquam quae dolore voluptatum pariatur rerum autem
+          totam quam soluta impedit sed.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab inventore,
+          atque quibusdam tempora consequatur consequuntur fuga sit, cum, quas
+          minus rerum fugiat quam minima esse expedita debitis dolore distinctio
+          impedit!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
+          asperiores alias inventore sed iste adipisci. Nobis fugiat distinctio
+          libero dignissimos harum odit quas tempora officia deserunt ut,
+          quaerat nisi atque.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum,
+          molestias fuga aliquid in, vel numquam nobis quas, sequi mollitia eos
+          eveniet itaque inventore? Ab necessitatibus nobis ratione cupiditate
+          repellat dolorum.
+        </p>
+      </div>
+    </>
+  );
 };
 
 export default Home;
