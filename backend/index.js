950e325df0d64f48a1f2eff589a96eb48677d43a
Nils <nilskrauseoffice@gmail.com>
2022-07-04 16:39:54 +0200

    Abfrage DB für Login

--- backend/index.js
+++ backend/index.js
@@ -19,11 +19,33 @@ app.post("/register", (req, res) => {
   const username = req.body.username;
   const email = req.body.email;
   const password = req.body.password;
+
   db.query(
     "INSERT INTO User (username, email, password) VALUES (?,?,?)",
     [username, email, password],
     (err, result) => {
       console.log(err);
+    }
+  );
+});
+
+app.post("/login", (req, res) => {
+  const password = req.body.password;
+  const email = req.body.email;
+
+  db.query(
+    "SELECT * FROM User WHERE password = ? AND email = ?",
+    [password, email],
+    (err, result) => {
+      if (err) {
+        res.send({ err: err });
+      }
+
+      if (result.length > 0) {
+        res.send(result);
+      } else {
+        res.send({ message: "Wrong Email and password combination" });
+      }
     }
   );
 });
