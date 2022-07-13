795a15c3a1dbd870f2df71460f62730aa24fece2
Jonas <code@ununun.de>
2022-07-13 21:33:10 +0200

    Frontend is now fetching posts from backend
    Add new "/post/:uuid" route to fetch single post

--- frontend/src/Pages/OfferOverview.jsx
+++ frontend/src/Pages/OfferOverview.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import SingleOffer from "../partials/SingleOffer";
 import classes from "./OfferOverview.module.css";
-
-const testoffers = require("../data/testoffers.json");
+import Axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
 
 const OfferOverview = () => {
+  const [posts, updatePosts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await Axios.get("http://localhost:3307/posts");
+      // For test purposes only: Pick a random index from the array and use this users uuid as a login param
+      updatePosts(res.data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className={classes.offerContainer}>
-      {testoffers.map((singleItem, key) => (
+      {posts.map((singleItem, key) => (
         <SingleOffer
           className={classes.offerCard}
           key={key} // should be replaced by the offer ID later
