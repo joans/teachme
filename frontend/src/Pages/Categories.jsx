e71e940ac94fe7f1cfefcc6d281c7c1f75dd4855
Jonas <code@ununun.de>
2022-08-14 16:52:55 +0200

    Basic Categories-Page with two-column layout
    and basic filtering works. It is still ugly tho

--- frontend/src/Pages/Categories.jsx
+++ frontend/src/Pages/Categories.jsx
@@ -1,10 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import Axios from "axios";
+import SingleOffer from "../partials/SingleOffer";
+import Card from "../UI/Card";
+import classes from "./Categories.module.css";
+import Button from "../UI/Button";
 
 const Categories = () => {
-  return <>
-  <h1>Categories</h1>
-  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, delectus. Qui doloribus eum necessitatibus voluptates asperiores repudiandae nisi ipsam, recusandae quod corporis eveniet magni eos dolorum non labore, pariatur nihil.</p>
-  </>;
+  const [origPosts, updateOrigPosts] = useState([]);
+  const [filteredPosts, updateFilteredPosts] = useState(origPosts);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await Axios.get("http://localhost:3307/categories_entries");
+        updateOrigPosts(res.data);
+      } catch (err) {
+        const errMsgBackend = err;
+        console.log(errMsgBackend);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const cateogryButtonOnClick = (e) => {
+    const selectedCategory = e.target.name;
+    const newPosts = origPosts.filter((post) => {
+      if (post.name === selectedCategory) {
+        return post;
+      } else {
+        return null;
+      }
+    });
+    updateFilteredPosts(newPosts);
+  };
+
+  return (
+    <Card>
+      <div className={classes["complete-container"]}>
+        <div className={classes["category-picker"]}>
+          Categories:
+          {origPosts.map((post) => (
+            <Button onClick={cateogryButtonOnClick} name={post.name}>
+              {post.displayName}
+            </Button>
+          ))}
+        </div>
+        <div className={classes["post-list"]}>
+          {filteredPosts.map((post, key) => (
+            <>
+              {post.posts.map((singlepost, key) => (
+                <SingleOffer key={key} item={singlepost} doTruncate={false} />
+              ))}
+            </>
+          ))}
+        </div>
+      </div>
+    </Card>
+  );
 };
 
 export default Categories;
--- frontend/src/partials/SingleOffer.jsx
+++ frontend/src/partials/SingleOffer.jsx
@@ -2,26 +2,31 @@ import React from "react";
 import { Link } from "react-router-dom";
 import classes from "./SingleOffer.module.css";
 
-const truncate = (string, maxlength = 30) => {
+const truncate = (string, maxlength = 30, doTruncate = true) => {
+  if (!doTruncate) {
+    return string;
+  }
   return string.length > maxlength
     ? `${string.substring(0, maxlength - 3)}...`
     : string;
 };
 
-const SingleOffer = ({ item, className }) => {
+const SingleOffer = ({ item, className, doTruncate = true }) => {
   // console.log(item);
   return (
     <div className={`${classes.singleCard} ${className}`}>
       <h2 className={classes.title}>
-        <Link to={`/offer/${item.uuid}`}>{truncate(item.title, 20)}</Link>
+        <Link to={`/offer/${item.uuid}`}>
+          {truncate(item.title, 20, doTruncate)}
+        </Link>
       </h2>
-      <p className={classes.body}>{truncate(item.body)}</p>
+      <p className={classes.body}>{truncate(item.body, 40, doTruncate)}</p>
       {/* <div className={classes.meta}> */}
       {/* <span className={classes.points}>
           {item.points} {item.points > 1 ? "Points" : "Point"}
         </span> */}
       <span className={classes.category}>
-        category: {item.category.displayName}
+        {item.category && item.category.displayName}
       </span>
       {/* </div> */}
     </div>
