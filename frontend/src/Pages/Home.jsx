1049f5972819ba5a6f3cda3c06205f43826e292d
Jonas <code@ununun.de>
2022-08-28 20:17:31 +0200

    Try to implement splash page

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import OfferOverview from "./OfferOverview";
+import SplashPage from "./SplashPage";
 
 const Home = () => {
   return (
     <>
       <div>
+        <SplashPage />
         <h1>Offers</h1>
         <OfferOverview />
       </div>
--- frontend/src/Pages/SplashPage.jsx
+++ frontend/src/Pages/SplashPage.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import classes from "./SplashPage.module.css";
+import { Link } from "react-router-dom";
+import Button from "../UI/Button";
 
 const SplashPage = () => {
-  return <div>SplashPage</div>;
+  return (
+    <div className={classes.team}>
+      <h1>
+        <span>
+          Welcome to <strong>Skillpact</strong>!
+        </span>
+      </h1>
+      <div className={classes.text}>
+        <span> Make learning an adventure.</span>
+        <br />
+        <span>
+          <strong> Skillpact </strong> is an online plattform that connects
+          people who want to develop personally while creating real connections.
+        </span>
+        <br />
+        <span> Share your skills and make an impact with Skillpact!</span>
+
+        <br />
+      </div>
+      <Button class="color-three">Sign Up</Button>
+    </div>
+  );
 };
 
 export default SplashPage;
